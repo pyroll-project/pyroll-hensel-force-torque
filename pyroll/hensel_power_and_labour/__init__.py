681db9b73b99ff97117d60f2ef26045bb7609bd7
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-12-21 10:50:34 +0100

    upd to 2.0

--- pyroll/hensel_power_and_labour/__init__.py
+++ pyroll/hensel_power_and_labour/__init__.py
@@ -1,16 +1,52 @@
-from pyroll.core import RollPass
-from pyroll.ui import Exporter, Reporter
+import numpy as np
+from pyroll.core import RollPass, Hook
 
-from . import specs
-from . import impls
+RollPass.roll_gap_ratio = Hook[float]()
+"""Calculate cross-section ratio used in Hensel-Poluchin master curves."""
 
-RollPass.plugin_manager.add_hookspecs(specs)
-RollPass.plugin_manager.register(impls)
+RollPass.rolling_efficiency = Hook[float]()
+"""Calculate rolling efficiency according to Hensel-Poluchin master curve."""
 
-from . import report
+RollPass.deformation_resistance = Hook[float]()
+"""Calculate deformation resistance according to Hensel-Poluchin master curve."""
 
-Reporter.plugin_manager.register(report)
+RollPass.lever_arm_coefficient = Hook[float]()
+"""Calculate lever coefficient according to Hensel-Poluchin master curve."""
 
-from . import export
 
-Exporter.plugin_manager.register(export)
+@RollPass.roll_gap_ratio
+def roll_gap_ratio(self: RollPass):
+    mean_cross_section = (self.in_profile.cross_section.area + 2 * self.out_profile.cross_section.area) / 3
+    return self.roll.contact_area / mean_cross_section
+
+
+@RollPass.rolling_efficiency
+def rolling_efficiency(self: RollPass):
+    inverse_efficiency = (0.9901 + 0.106 * self.roll_gap_ratio + 0.0283 * self.roll_gap_ratio ** 2
+                          + 1.5718 * np.exp(-2.4609 * self.roll_gap_ratio)
+                          + 0.3117 * np.exp(-15.625 * self.roll_gap_ratio ** 2))
+    return inverse_efficiency ** -1
+
+
+@RollPass.deformation_resistance
+def deformation_resistance(self: RollPass):
+    mean_flow_stress = (self.in_profile.flow_stress + 2 * self.out_profile.flow_stress) / 3
+    return mean_flow_stress / self.rolling_efficiency
+
+
+@RollPass.lever_arm_coefficient
+def lever_arm_coefficient(self: RollPass):
+    mean_temperature = (self.in_profile.temperature + self.out_profile.temperature) / 2
+    return ((np.exp(-0.6 * self.roll_gap_ratio) + 0.076 * self.roll_gap_ratio)
+            * self.velocity ** 0.005 * np.exp(
+                -0.0003 * (mean_temperature - 273.15 - 900)))
+
+
+@RollPass.roll_force
+def roll_force(self: RollPass):
+    return self.deformation_resistance * self.roll.contact_area
+
+
+@RollPass.Roll.roll_torque
+def roll_torque(self: RollPass.Roll):
+    return self.roll_pass().roll_force * self.contact_length * self.roll_pass().lever_arm_coefficient
--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,24 +1,57 @@
+import logging
 from pathlib import Path
 
-from pyroll.core import solve
-from pyroll.ui import Exporter, Reporter
+from pyroll.core import Profile, PassSequence, RollPass, Roll, CircularOvalGroove, Transport, RoundGroove
 
 
-def test_solve(tmp_path: Path):
-    import pyroll.ui.cli.res.input_trio as input_py
+def test_solve(tmp_path: Path, caplog):
+    caplog.set_level(logging.DEBUG, logger="pyroll")
+
     import pyroll.hensel_power_and_labour
 
-    sequence = input_py.sequence
+    in_profile = Profile.round(
+        diameter=30e-3,
+        temperature=1200 + 273.15,
+        strain=0,
+        material=["C45", "steel"],
+        flow_stress=100e6
+    )
 
-    solve(sequence, input_py.in_profile)
-    print()
+    sequence = PassSequence([
+        RollPass(
+            label="Oval I",
+            roll=Roll(
+                groove=CircularOvalGroove(
+                    depth=8e-3,
+                    r1=6e-3,
+                    r2=40e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+        ),
+        Transport(
+            label="I => II",
+            duration=1
+        ),
+        RollPass(
+            label="Round II",
+            roll=Roll(
+                groove=RoundGroove(
+                    r1=1e-3,
+                    r2=12.5e-3,
+                    depth=11.5e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+        ),
+    ])
 
-    rendered = Reporter().render(sequence)
-    report_file = tmp_path / "report.html"
-    report_file.write_text(rendered)
-    print(report_file)
-
-    exported = Exporter().export(sequence, "csv")
-    export_file = tmp_path / "export.csv"
-    export_file.write_bytes(exported)
-    print(export_file)
+    try:
+        sequence.solve(in_profile)
+    finally:
+        print("\nLog:")
+        print(caplog.text)
